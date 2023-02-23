c92ee79b587dbe9699b716be2002c5aedf83ba2b
Ariadna Frias <ariadna.812@gmail.com>
2023-02-23 21:11:07 +0100

    Update index ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,50 @@
-/* eslint-disable @typescript-eslint/object-curly-spacing */
-/* eslint-disable @typescript-eslint/indent */
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import http from 'http';
+import number from 'inquirer/lib/prompts/number.js';
 import url from 'url';
+import { calculadora } from './calculator.js';
 
 const PORT = process.env.PORT || '4300';
 
 const server = http.createServer((req, resp) => {
-  switch (req.method) {
-    case 'GET':
-      if (!req.url) {
-        server.emit('error', new Error('Invalid URL'));
-        return;
-      }
-
-      // eslint-disable-next-line no-case-declarations
-      const { pathname } = url.parse(req.url);
-      resp.write('Hello world: estos son tus datos de ' + pathname);
-      break;
-    case 'POST':
-      break;
-    case 'PATCH':
-    case 'DELETE':
-      resp.write('Hello world, de momento no esta implementado ' + req.method);
-      break;
-    default:
-      resp.write('Hello world, no conozco el método ' + req.method);
-      break;
+  if (!req.url) {
+    server.emit('error', new Error('Error 404'));
+    return;
   }
+
+  const { search, pathname } = url.parse(req.url);
+  const urlParams = new URLSearchParams(search!);
+
+  if (pathname !== '/calculator') {
+    server.emit('error', new Error('Error - path not found'));
+    resp.write(`<p>Error - path not found</p>`);
+    return;
+  }
+
+  /* If (isNaN(num1+1) || isNaN(num2+1)) {
+    server.emit(
+      'error',
+      new Error('Error - el dato introducido no es un número')
+    );
+    resp.write(`<p>Error - el dato introducido no es un número</p>`);
+    return;
+  } */
+
+  const num1 = urlParams.get('a');
+  const num2 = urlParams.get('b');
+
+  const answers = calculadora(num1, num2);
+
+  resp.writeHead(200, { 'Content-Type': 'text/html' });
+  resp.write(`<h1>Calculadora</h1>
+  <h3>Resultados:</h2>
+   <p>${num1} + ${num2} = ${answers.sum}</p>
+   <p>${num1} - ${num2} = ${answers.rest}</p>
+    <p>${num1} * ${num2} = ${answers.mult}</p>
+      <p>${num1} / ${num2} = ${answers.div}</p>
+   `);
 
   resp.end();
 });
